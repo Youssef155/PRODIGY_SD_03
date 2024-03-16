530e32f1b766ffa21aca3183640e8a311e708574
Youssef George <yousifkhaleel00@gmail.com>
2024-03-16 10:46:09 +0200

    MVC For Creating

---
+++ ContactManagementSystem/Controllers/ContactController.cs
@@ -0,0 +1,43 @@
+﻿using ContactManagementSystem.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContactManagementSystem.Controllers
+{
+    public class ContactController : Controller
+    {
+        private static List<ContactInfoViewModel> contacts = new List<ContactInfoViewModel>();
+
+        // GET: ContactController
+        public ActionResult Index()
+        {
+            return View(contacts);
+        }
+
+        // GET: ContactController/Create
+        public ActionResult Create()
+        {
+            var contVM = new ContactInfoViewModel();
+            return View(contVM);
+        }
+
+        public ActionResult CreateContact(ContactInfoViewModel contactInfoViewModel)
+        {
+            contacts.Add(contactInfoViewModel);
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: ContactController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // GET: ContactController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+    }
+}
---
+++ ContactManagementSystem/Models/ContactInfoViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace ContactManagementSystem.Models
+{
+    public class ContactInfoViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string PhoneNumber { get; set; } = string.Empty;
+
+    }
+}
